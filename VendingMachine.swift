20f08b97066073f3a314a4034057ed892f9e5002
Justin Needham <justin.needham@alumnimail.pepperdine.edu>
2019-02-06 16:18:32 -0700

    add data delegate

--- VendingMachine.swift
+++ VendingMachine.swift
@@ -5,7 +5,7 @@
 
 import Foundation
 
-enum VendingSelection {
+enum VendingSelection: String {
     case soda
     case dietSoda
     case chips
@@ -20,6 +20,84 @@ enum VendingSelection {
     case gum
 }
 
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
 protocol VendingMachine {
-    var selection: [String] { get }
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws
+    func deposit(_ amount: Double)
+}
+
+struct Item: VendingItem {
+    let price: Double
+    var quantity: Int
+}
+
+enum InventoryError: Error {
+    case invalidResource
+    case conversionFailure
+    case invalidSelection
+}
+
+class PListConverter {
+    static func dictionary(fromFile name: String, ofType type: String) throws -> [String: AnyObject] {
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw InventoryError.invalidResource
+        }
+
+        guard let dictionary = NSDictionary(contentsOfFile: path) as? [String: AnyObject] else {
+            throw InventoryError.conversionFailure
+        }
+
+        return dictionary
+    }
+}
+
+class InventoryUnarchiver{
+    static func vendingInventory(fromDictionary dictionary: [String: AnyObject]) throws -> [VendingSelection: VendingItem] {
+        var inventory: [VendingSelection: VendingItem] = [:]
+
+        for (key, value) in dictionary {
+            if let itemDictionary = value as? [String: Any],
+               let price = itemDictionary["price"] as? Double,
+               let quantity = itemDictionary["quantity"] as? Int {
+                let item = Item(price: price, quantity: quantity)
+                guard let selection = VendingSelection(rawValue: key) else {
+                    throw InventoryError.invalidSelection
+                }
+                inventory.updateValue(item, forKey: selection)
+            }
+        }
+        return inventory
+    }
+}
+
+class FoodVendingMachine: VendingMachine {
+    let selection: [VendingSelection] = [
+        .soda, .dietSoda, .chips,
+        .cookie, .wrap, .sandwich,
+        .candyBar, .popTart, .water,
+        .fruitJuice, .sportsDrink, .gum
+    ]
+    var inventory: [VendingSelection: VendingItem]
+    var amountDeposited: Double = 10.0
+
+    required init(inventory: [VendingSelection: VendingItem]) {
+        self.inventory = inventory
+    }
+
+    func vend(_ quantity: Int, _ selection: VendingSelection) throws {
+
+    }
+
+    func deposit(_ amount: Double) {
+
+    }
 }
